5f1be2e697a37ac7b8d69bed6d75bd3cc1796177
Коперский Андрей Валерьевич <a.v.koperskiy@otpbank.ru>
2019-12-26 11:54:39 +0300

    skybonds test

--- skybonds/fraction.js
+++ skybonds/fraction.js
@@ -6,7 +6,10 @@ function pow(x, n) {
   return result;
 }
 
-function fractionMutable(parts, accuracy = 4) {
+function fractionMutable(parts, accuracy = 3) {
+  if (parts.length > 5e6) {
+    throw new Error("fractionMutable: To big input array (max 5 000 000)");
+  }
   const mul = pow(10, accuracy + 2);
   const div = pow(10, accuracy);
   let acc1 = 0;
@@ -35,52 +38,53 @@ function fractionMutable(parts, accuracy = 4) {
 }
 
 /** TEST */
-const code = document.getElementById('code');
-const result = document.getElementById('result');
-code.innerText = `
-/**
- * Заменяет доли в исходном массиве на их процентное выражение.
- * Алгоритмическая сложность O(1) - два прохода на любое количество входных данных.
- * Ипользование памяти O(1) - входный массив мутируется, новые структуры данных не создаются.
- * @param {Array[string]} parts 
- * @param {number} accuracy Количество знаков после заяптой в рассчитанных процентах.
- * /
-${fractionMutable.toString()}
-`;
-
-const TEST_SIZE = 1e4;
+const searchParams = new URLSearchParams(window.location.search);
+const size = searchParams.has("size") ? searchParams.get("size") : 1e4;
+const accuracy = searchParams.has("accuracy")
+  ? parseInt(searchParams.get("accuracy"))
+  : 3;
 const testArr = [];
 
-for (let i = 0; i < TEST_SIZE; i += 1) {
-  const rnd = Math.round(Math.random() * 1000) / 10;
-  const n = rnd % 2 ? rnd : rnd / 10;
-  testArr.push(n.toString());
-}
-
+const result = document.getElementById("result");
 result.innerText = `
-Длина входного массива: ${TEST_SIZE.toString().replace(
-  /\d{1,3}(?=(\d{3})+(?!\d))/g,
-  '$& '
-)}
+Длина входного массива: ${size
+  .toString()
+  .replace(/\d{1,3}(?=(\d{3})+(?!\d))/g, "$& ")}
+Точность: ${accuracy}
+...создается входной массив`;
+
+window.setTimeout(() => {
+  for (let i = 0; i < size; i += 1) {
+    const rnd = Math.round(Math.random() * 1000) / 10;
+    const n = rnd % 2 ? rnd : rnd / 10;
+    testArr.push(n.toString());
+  }
+
+  result.innerText += `
 Пример входного массива ${JSON.stringify(
-  testArr.slice(0, 5).concat('...'),
+  testArr.slice(0, 5).concat("..."),
   null,
   2
 )}
 ...выполняется тест`;
-const startTs = window.performance.now();
+  const startTs = window.performance.now();
 
-window.setTimeout(() => {
-  fractionMutable(testArr);
+  window.setTimeout(() => {
+    try {
+      fractionMutable(testArr, accuracy);
 
-  result.innerText += `
+      result.innerText += `
 ...готово
 Время обработки входного массива: ${window.performance.now() - startTs}ms
 Накопленная погрешность округлений: ${testArr[testArr.length - 1]};
 Пример результирующего массива ${JSON.stringify(
-    testArr.slice(0, 5).concat('...'),
-    null,
-    2
-  )}
-`;
+  testArr.slice(0, 5).concat("..."),
+  null,
+  2
+)}
+      `;
+    } catch (err) {
+      result.innerText += `\nError ${err.message}`;
+    }
+  }, 300);
 }, 300);
